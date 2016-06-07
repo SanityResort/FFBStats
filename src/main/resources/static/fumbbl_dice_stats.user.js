e3cc2a3b2101505b3982bdc9adaa8b230485a681
Christian Huber <christian.huber@sueddeutsche.de>
2016-06-07 23:18:43 +0200

    Retry setting link after failure. Removed replay id from matcher... d'oh

--- src/main/resources/static/fumbbl_dice_stats.user.js
+++ src/main/resources/static/fumbbl_dice_stats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FUMBBL Dice Stats
 // @namespace    http://ffbstats.butterbrot.org/
-// @version      0.2
+// @version      0.3
 // @author       Candlejack
 // @match        http*://fumbbl.com/FUMBBL.php?page=match&id=*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
@@ -9,14 +9,14 @@
 
 function setLink() {
     try {
-        var linkTag =  $('a[href*=\'/ffblive.jnlp?replay=860124\']');
+        var linkTag =  $('a[href*=\'/ffblive.jnlp?replay=\']');
         var link = $(linkTag).attr('href');
         var id = link.split('=')[1];
         var parent = $(linkTag).parent();
         parent.html(parent.html() + '<br><a href="https://ffbstats.herokuapp.com/stats/'+id+'" target="_blank">Dice Stats</a>');
     } catch (e) {
-        console.log("Could not set link", e);
-
+        console.log("Could not set link, retry in 2 seconds", e);
+        setTimeout(setLink, 2000);
     }
 }
 
