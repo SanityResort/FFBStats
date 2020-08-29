0bef63794acb96544692d11abd3bc027f9093ad7
Christian Huber <huberc@butterbrot.org>
2020-08-29 17:26:06 +0200

    Add new evaluator

---
+++ src/main/java/org/butterbrot/ffb/stats/evaluation/stats/ReportRiotousRookiesEvaluator.java
@@ -0,0 +1,27 @@
+package org.butterbrot.ffb.stats.evaluation.stats;
+
+import com.balancedbytes.games.ffb.report.IReport;
+import com.balancedbytes.games.ffb.report.ReportRiotousRookies;
+import com.balancedbytes.games.ffb.report.ReportSwarmingRoll;
+import org.butterbrot.ffb.stats.model.StatsCollection;
+
+public class ReportRiotousRookiesEvaluator extends Evaluator<ReportRiotousRookies> {
+
+    private StatsCollection collection;
+
+    public ReportRiotousRookiesEvaluator(StatsCollection collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public void evaluate(IReport report) {
+        ReportRiotousRookies rookiesRoll = (ReportRiotousRookies) report;
+        for (int roll: rookiesRoll.getRoll()) {
+            if (rookiesRoll.isHomeTeam()) {
+                collection.getHome().addSingleRoll(roll * 2);
+            } else {
+                collection.getAway().addSingleRoll(roll * 2);
+            }
+        }
+    }
+}
