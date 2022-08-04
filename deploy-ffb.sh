d5cb12b866c2bc3333ea6a534d9b8db7ac08d32e
Christian Huber <huberc@butterbrot.org>
2022-08-04 14:44:03 +0200

    Clean local repo before update

--- deploy-ffb.sh
+++ deploy-ffb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+rm -rf ./repo/com
+
 mvn deploy:deploy-file -Dfile=$1 -DgroupId=com.fumbbl.ffb.client -DartifactId=FantasyFootballClient -Dversion=$2 -Dpackaging=jar -DrepositoryId=internal -Durl=file://repo
