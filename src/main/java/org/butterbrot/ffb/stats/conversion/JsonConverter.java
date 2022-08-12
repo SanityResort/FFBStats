ad42c29387fda627ba60cca9e3086ac93b558aa7
Christian Huber <huberc@butterbrot.org>
2022-08-12 18:44:28 +0200

    Further hardening

--- src/main/java/org/butterbrot/ffb/stats/conversion/JsonConverter.java
+++ src/main/java/org/butterbrot/ffb/stats/conversion/JsonConverter.java
@@ -12,6 +12,8 @@ import com.google.gson.JsonObject;
 import org.butterbrot.ffb.stats.adapter.ExposingInjuryReport;
 import org.butterbrot.ffb.stats.evaluation.stats.StatsCollector;
 import org.butterbrot.ffb.stats.model.StatsCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,6 +24,9 @@ import static org.butterbrot.ffb.stats.Constants.*;
 
 @Service
 public class JsonConverter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JsonConverter.class);
+
     public StatsCollection convert(JsonObject root, String replayId) {
         EvaluationFactorySource factorySource = new EvaluationFactorySource();
         NetCommandFactory factory = new NetCommandFactory(factorySource);
@@ -36,8 +41,12 @@ public class JsonConverter {
             JsonElement element = it.next();
             String id = element.getAsJsonObject().get(FIELD_NET_COMMAND_ID).getAsString();
             if (NetCommandId.SERVER_MODEL_SYNC.getName().equals(id)) {
-                replayCommands
-                        .add((ServerCommand) factory.forJsonValue(game.getRules(), JsonValue.readFrom(element.toString())));
+                try {
+                    replayCommands
+                      .add((ServerCommand) factory.forJsonValue(game.getRules(), JsonValue.readFrom(element.toString())));
+                } catch (Exception e) {
+                    logger.error("Could not create replay command: " + element, e);
+                }
             }
         }
 
--- src/main/java/org/butterbrot/ffb/stats/evaluation/stats/bb2016/StatsCollector.java
+++ src/main/java/org/butterbrot/ffb/stats/evaluation/stats/bb2016/StatsCollector.java
@@ -15,108 +15,109 @@ import org.butterbrot.ffb.stats.evaluation.stats.StatsState;
 import org.butterbrot.ffb.stats.evaluation.stats.common.UploadEvaluator;
 import org.butterbrot.ffb.stats.evaluation.turnover.TurnOverFinder;
 import org.butterbrot.ffb.stats.model.StatsCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StatsCollector extends org.butterbrot.ffb.stats.evaluation.stats.StatsCollector<ReportPoInjury> {
 
-    //private static final Logger logger = LoggerFactory.getLogger(StatsCollector.class);
+	private static final Logger logger = LoggerFactory.getLogger(StatsCollector.class);
 
-    public StatsCollector() {
-        this(new ArrayList<>());
-    }
+	public StatsCollector(List<ServerCommand> replayCommands) {
+		super(replayCommands);
+		evaluators.add(new ApothecaryRollEvaluator(collection));
+		evaluators.add(new InjuryEvaluator(collection, state));
+		evaluators.add(new KickoffExtraReRollEvaluator(collection));
+		evaluators.add(new KickoffPitchInvasionEvaluator(collection, state));
+		evaluators.add(new KickoffThrowARockEvaluator(collection));
+		evaluators.add(new SpectatorsEvaluator(state));
+		evaluators.add(new TurnEndEvaluator(collection, state));
+		evaluators.add(new UploadEvaluator(collection));
+	}
 
-    public StatsCollector(List<ServerCommand> replayCommands) {
-        super(replayCommands);
-        evaluators.add(new ApothecaryRollEvaluator(collection));
-        evaluators.add(new InjuryEvaluator(collection, state));
-        evaluators.add(new KickoffExtraReRollEvaluator(collection));
-        evaluators.add(new KickoffPitchInvasionEvaluator(collection, state));
-        evaluators.add(new KickoffThrowARockEvaluator(collection));
-        evaluators.add(new SpectatorsEvaluator(state));
-        evaluators.add(new TurnEndEvaluator(collection, state));
-        evaluators.add(new UploadEvaluator(collection));
-    }
+	public StatsCollection evaluate(String replayId) {
+		collection.setReplayId(replayId);
 
-    public StatsCollection evaluate(String replayId) {
-        collection.setReplayId(replayId);
+		for (ServerCommand command : replayCommands) {
+			if (command instanceof ServerCommandModelSync) {
+				//logger.info(new Gson().toJson(command));
 
-        for (ServerCommand command : replayCommands) {
-            if (command instanceof ServerCommandModelSync) {
-                //logger.info(new Gson().toJson(command));
+				ServerCommandModelSync modelSync = (ServerCommandModelSync) command;
 
-                ServerCommandModelSync modelSync = (ServerCommandModelSync) command;
+				ReportList reportList = modelSync.getReportList();
+				for (IReport report : reportList.getReports()) {
+					try {
+						report.diceStats(collection.getGame()).forEach(collection::addStat);
+						//  logger.info(new Gson().toJson(report));
+						if (!TurnMode.KICKOFF_RETURN.equals(state.getTurnMode())) {
+							if (state.isActionTurn()) {
+								turnOverFinder.add(report);
+							}
+							for (Evaluator<?> evaluator : evaluators) {
+								if (evaluator.handles(report)) {
+									evaluator.evaluate(report);
+									break;
+								}
+							}
+						}
+					} catch (Exception e) {
+						logger.error("Could not evaluate report: " + report.getId(), e);
+					}
+				}
 
-                ReportList reportList = modelSync.getReportList();
-                for (IReport report : reportList.getReports()) {
-                    report.diceStats(collection.getGame()).forEach(collection::addStat);
-                  //  logger.info(new Gson().toJson(report));
-                    if (!TurnMode.KICKOFF_RETURN.equals(state.getTurnMode())) {
-                        if (state.isActionTurn()) {
-                            turnOverFinder.add(report);
-                        }
-                        for (Evaluator<?> evaluator : evaluators) {
-                            if (evaluator.handles(report)) {
-                                evaluator.evaluate(report);
-                                break;
-                            }
-                        }
-                    }
-                }
+				boolean newTurnValuesSet = false;
 
-                boolean newTurnValuesSet = false;
+				for (ModelChange change : modelSync.getModelChanges().getChanges()) {
+					//logger.info(new Gson().toJson(change));
+					if (ModelChangeId.GAME_SET_HOME_PLAYING == change.getChangeId()) {
+						state.setHomePlaying((boolean) change.getValue());
+						turnOverFinder.setHomeTeamActive(state.isHomePlaying());
+					}
 
-                for (ModelChange change : modelSync.getModelChanges().getChanges()) {
-                    //logger.info(new Gson().toJson(change));
-                    if (ModelChangeId.GAME_SET_HOME_PLAYING == change.getChangeId()) {
-                        state.setHomePlaying((boolean) change.getValue());
-                        turnOverFinder.setHomeTeamActive(state.isHomePlaying());
-                    }
+					if (ModelChangeId.GAME_SET_TURN_MODE == change.getChangeId()) {
+						state.setTurnMode((TurnMode) change.getValue());
+						newTurnValuesSet = true;
+					}
 
-                    if (ModelChangeId.GAME_SET_TURN_MODE == change.getChangeId()) {
-                        state.setTurnMode((TurnMode) change.getValue());
-                        newTurnValuesSet = true;
-                    }
+					if (ModelChangeId.TURN_DATA_SET_TURN_NR == change.getChangeId()) {
+						state.setTurnNumber((int) change.getValue());
+						newTurnValuesSet = true;
+					}
+				}
 
-                    if (ModelChangeId.TURN_DATA_SET_TURN_NR == change.getChangeId()) {
-                        state.setTurnNumber((int) change.getValue());
-                        newTurnValuesSet = true;
-                    }
-                }
+				state.setActionTurn(TurnMode.BLITZ == state.getTurnMode() || TurnMode.REGULAR == state.getTurnMode());
 
-                state.setActionTurn(TurnMode.BLITZ == state.getTurnMode() || TurnMode.REGULAR == state.getTurnMode());
+				if (newTurnValuesSet && state.isActionTurn() && state.isNewTurn()) {
+					turnOverFinder.findTurnover().ifPresent(collection::addTurnOver);
+					turnOverFinder.reset();
+					state.setLastTurn(collection.addTurn(state.isHomePlaying(), state.getTurnMode(), state
+						.getTurnNumber()));
+				}
+			}
+		}
 
-                if (newTurnValuesSet && state.isActionTurn() && state.isNewTurn()){
-                    turnOverFinder.findTurnover().ifPresent(collection::addTurnOver);
-                    turnOverFinder.reset();
-                    state.setLastTurn(collection.addTurn(state.isHomePlaying(), state.getTurnMode(), state
-                            .getTurnNumber()));
-                }
-            }
-        }
+		// ugly hack to evaluate master chef rolls of the current active half before the game ends.
+		// during the loop master chef rolls are evaluated before a new half is started
+		// it can't happen when the report appears as the first half start is not reported but the others are
+		// this way is the only remotely consistent one I found so far
+		halfEvaluator.evaluate(new ReportStartHalf(0));
 
-        // ugly hack to evaluate master chef rolls of the current active half before the game ends.
-        // during the loop master chef rolls are evaluated before a new half is started
-        // it can't happen when the report appears as the first half start is not reported but the others are
-        // this way is the only remotely consistent one I found so far
-        halfEvaluator.evaluate(new ReportStartHalf(0));
+		return collection;
+	}
 
-        return collection;
-    }
+	@Override
+	protected org.butterbrot.ffb.stats.adapter.PlayerActionMapping createPlayerActionMapping() {
+		return new PlayerActionMapping();
+	}
 
-    @Override
-    protected org.butterbrot.ffb.stats.adapter.PlayerActionMapping createPlayerActionMapping() {
-        return new PlayerActionMapping();
-    }
+	@Override
+	protected StatsState<ReportPoInjury> createStatsState() {
+		return new org.butterbrot.ffb.stats.evaluation.stats.bb2016.StatsState();
+	}
 
-    @Override
-    protected StatsState<ReportPoInjury> createStatsState() {
-        return new org.butterbrot.ffb.stats.evaluation.stats.bb2016.StatsState();
-    }
-
-    @Override
-    protected TurnOverFinder createTurnOverFinder() {
-        return new org.butterbrot.ffb.stats.evaluation.turnover.bb2016.TurnOverFinder();
-    }
+	@Override
+	protected TurnOverFinder createTurnOverFinder() {
+		return new org.butterbrot.ffb.stats.evaluation.turnover.bb2016.TurnOverFinder();
+	}
 }
