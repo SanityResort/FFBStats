28635d9eacbcaf647559232680e63448640ac3b1
Christian Huber <huberc@butterbrot.org>
2024-01-27 18:28:55 +0100

    Remove obsolete annotations

--- src/main/java/org/butterbrot/ffb/stats/StatsStarter.java
+++ src/main/java/org/butterbrot/ffb/stats/StatsStarter.java
@@ -1,12 +1,8 @@
 package org.butterbrot.ffb.stats;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
 @SpringBootApplication
 public class StatsStarter {
 
