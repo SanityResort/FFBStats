b4e8e33e30a72805b75d62595908c575aa761a2a
Christian Huber <huberc@butterbrot.org>
2024-06-06 20:23:57 +0200

    Bloodlust knockdowns after block or blitz did create additional block entries

--- src/main/java/org/butterbrot/ffb/stats/evaluation/stats/bb2020/InjuryEvaluator.java
+++ src/main/java/org/butterbrot/ffb/stats/evaluation/stats/bb2020/InjuryEvaluator.java
@@ -1,5 +1,6 @@
 package org.butterbrot.ffb.stats.evaluation.stats.bb2020;
 
+import com.fumbbl.ffb.SendToBoxReason;
 import com.fumbbl.ffb.report.IReport;
 import com.fumbbl.ffb.report.bb2020.ReportInjury;
 import com.fumbbl.ffb.util.ArrayTool;
@@ -8,14 +9,24 @@ import org.butterbrot.ffb.stats.evaluation.stats.Evaluator;
 import org.butterbrot.ffb.stats.evaluation.stats.StatsState;
 import org.butterbrot.ffb.stats.model.StatsCollection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InjuryEvaluator extends Evaluator<ReportInjury> {
 
+    private final Set<SendToBoxReason> nonBlockReasons = new HashSet<>();
     private final StatsCollection collection;
     private final StatsState<ReportPoInjury> state;
 
     public InjuryEvaluator(StatsCollection collection, StatsState<ReportPoInjury> state) {
         this.collection = collection;
         this.state = state;
+        nonBlockReasons.add(SendToBoxReason.BITTEN);
+        nonBlockReasons.add(SendToBoxReason.CROWD_PUSHED);
+        nonBlockReasons.add(SendToBoxReason.FOULED);
+        nonBlockReasons.add(SendToBoxReason.DODGE_FAIL);
+        nonBlockReasons.add(SendToBoxReason.JUMP_FAIL);
+        nonBlockReasons.add(SendToBoxReason.TRAP_DOOR_FALL);
     }
 
     @Override
@@ -35,7 +46,7 @@ public class InjuryEvaluator extends Evaluator<ReportInjury> {
                 collection.addInjuryRoll(injury.getInjuryRoll(), injury.getDefenderId());
             }
         }
-        if (state.getActivePlayer() != null && state.getCurrentBlockRoll() != null) {
+        if (state.getActivePlayer() != null && state.getCurrentBlockRoll() != null && !nonBlockReasons.contains(injury.getInjuryType().injuryType().sendToBoxReason())) {
             collection.addBlockKnockDown(state.getCurrentBlockRoll().getBlockRoll().length, injury.getDefenderId(),
                     state.getCurrentBlockRoll().getChoosingTeamId(), state.getActivePlayer());
         }
