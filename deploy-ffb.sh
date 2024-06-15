1275a610140967ba989107483482394cc0c5df54
Christian Huber <huberc@butterbrot.org>
2024-06-16 00:42:05 +0200

    Revert to local repo usage

--- deploy-ffb.sh
+++ deploy-ffb.sh
@@ -2,4 +2,4 @@
 
 rm -rf ./repo/com
 
-mvn deploy:deploy-file -Dfile=$1 -DgroupId=com.fumbbl.ffb.client -DartifactId=FantasyFootballClient -Dversion=$2 -Dpackaging=jar -DrepositoryId=internal -Durl=file://repo
+mvn deploy:deploy-file -Dfile=$1 -DgroupId=com.fumbbl.ffb -DartifactId=ffb-client-logic -Dversion=$2 -Dpackaging=jar -DrepositoryId=internal -Durl=file://repo
