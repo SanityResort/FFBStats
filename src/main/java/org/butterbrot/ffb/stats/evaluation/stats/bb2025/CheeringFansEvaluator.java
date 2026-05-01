76f697254d4c8b2bc874501ead0ba0adf7e0eae4
Christian Linse <christian@butterbrot.org>
2026-05-01 18:05:23 +0200

    Add cheering fans evaluator for 2025

---
+++ src/main/java/org/butterbrot/ffb/stats/evaluation/stats/bb2025/CheeringFansEvaluator.java
@@ -0,0 +1,21 @@
+package org.butterbrot.ffb.stats.evaluation.stats.bb2025;
+
+import com.fumbbl.ffb.report.IReport;
+import com.fumbbl.ffb.report.bb2025.ReportCheeringFans;
+import org.butterbrot.ffb.stats.evaluation.stats.Evaluator;
+import org.butterbrot.ffb.stats.model.StatsCollection;
+
+public class CheeringFansEvaluator extends Evaluator<ReportCheeringFans> {
+
+	private final StatsCollection collection;
+
+	public CheeringFansEvaluator(StatsCollection collection) {
+		this.collection = collection;
+	}
+
+	@Override
+	public void evaluate(IReport report) {
+		ReportCheeringFans cheeringFans = (ReportCheeringFans) report;
+		collection.addKickOffRolls(new int[]{cheeringFans.getRollHome()}, new int[]{cheeringFans.getRollAway()});
+	}
+}
